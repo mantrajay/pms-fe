874456d67f74b519b69bbf944fa0edf1f97c32bd
jocumen <jocumen@YND103022TG-JO.egg.ph>
2021-02-18 11:43:59 +0800

    Change env

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
+  generate: {
+    dir: '../production/fe'
+  },
   head: {
     titleTemplate: '%s',
     title: 'Pangasinan Medical Society',
